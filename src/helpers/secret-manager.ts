f75cef2c08c9d9195ec450b1838cf6d75f4dc0ab
Elton Doehnert <eltonfd@gmail.com>
2023-02-09 17:51:30 -0300

    added env variables to use gcp secrets

--- src/helpers/secret-manager.ts
+++ src/helpers/secret-manager.ts
@@ -4,12 +4,11 @@ const client = new SecretManagerServiceClient();
 
 export async function accessSecret(name, version = 'latest') {
   try {
-    if (!process.env.GCP_PROJECT_ID) {
-      throw 'Please set the GCP_PROJECT_ID env var.';
-    }
+    // if (!process.env.GCP_PROJECT_ID) {
+    //   throw 'Please set the GCP_PROJECT_ID env var.';
+    // }
     const fullName =
-      `projects/${process.env.GCP_PROJECT_ID}/secrets/` +
-      `${name}/versions/${version}`;
+      `projects/soy-serenity-368016/secrets/` + `${name}/versions/${version}`;
 
     const [response] = await client.accessSecretVersion({ name: fullName });
     const payload = response.payload.data.toString();
